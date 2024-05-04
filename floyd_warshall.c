e49e8296d4fc82ebd6a232a03f7fd95f366d3dfc
dhruvsheh <dhruvgr80905@gmail.com>
2024-05-04 18:27:10 +0530

    p2

--- floyd_warshall.c
+++ floyd_warshall.c
@@ -13,4 +13,13 @@ void floydWarshall(int d[v][v]){//as it is an even matrix
             }
         }
     }
+    printf("all pair shortest paths are:");
+    for (i = 0; i < v; i++){
+        for (j = 0; j < v; j++){
+            if (d[i][j] == INF)
+                printf("%s ", "INF");
+            else
+                printf("%d ", d[i][j]);
+        }
+    }
 }
