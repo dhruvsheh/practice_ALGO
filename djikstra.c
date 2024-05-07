a8082b521fd0c35c935fe59f89104e3fcf4456c2
dhruvsheh <dhruvgr80905@gmail.com>
2024-05-07 18:43:29 +0530

    lcs done

---
+++ djikstra.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<conio.h>
+int n,source,V[20],parent[20],dist[20],i,j,k; //V[20] to show if a vertex has been visited or not yet
+int graph[6][6]={{0, 4, 0, 0, 0, 0},
+                {4, 0, 8, 0, 0, 0},
+                {0, 8, 0, 7, 0, 4},
+                {0, 0, 7, 0, 9, 14},
+                {0, 0, 0, 9, 0, 10},
+                {0, 0, 4, 14, 10, 0}};
+void initialise_single_source(){
+    for(i=0;i<n;i++){
+        dist[i]=10000;
+        parent[i]=-1;
+    }
+    dist[source]=0;
+}
+int find_minimum(){                                      
+    int min = 1000;
+    int temp = -1;
+    for(i=0;i<n;i++){
+        if(dist[i]<min && V[i] == 0){
+            temp = i;
+            min = dist[i];
+        }
+    }
+    V[temp] = 1;
+    return temp;
+}
+void relax(int u,int v){
+    if(dist[v]>dist[u]+graph[u][v]){
+        dist[v]=dist[u]+graph[u][v]
+        parent[v]=u;
+    }
+}
+
+void djikstra(){
+    initialise_single_source();
+    for(i=0;i<n;i++){
+        int u=find_minimum();
+        for(j=0;j<n;j++){
+            if(graph[u][j]!=0&&V[j]==0){
+                relax(u,j);
+            }
+        }
+    }
+}
+void display(){
+    
+}
